d340c275efc1f63b94bc03adb280ac15146b4f9e
s28502 <s28502@pjwstk.edu.pl>
2024-03-18 13:21:26 +0100

    "Container class"

---
+++ APBD_3/APBD_2/Container.cs
@@ -0,0 +1,53 @@
+﻿namespace APBD_3;
+
+public class Container
+{
+    private double _weight;
+    private double _height;
+    private double _containerWeight;
+    private double _depth;
+    private double _maxWeight;
+    private double _serialNumber;
+    private static int counter = 0;
+    
+    
+    public double Weight
+    {
+        get { return _weight;}
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Weight cannot be less than zero");
+            }
+
+            _weight = value;
+        }
+    }
+    public double Height
+    {
+        get { return _height;}
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Height cannot be less than zero");
+            }
+
+            _height = value;
+        }
+    }
+    public double ContainerWeight
+    {
+        get { return _containerWeight;}
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Container weight cannot be less than zero");
+            }
+
+            _containerWeight = value;
+        }
+    }
+}
--- APBD_3/APBD_2/Program.cs
+++ APBD_3/APBD_2/Program.cs
@@ -5,8 +5,3 @@
         
     }
 }
-
-public class Container
-{
-    
-}
